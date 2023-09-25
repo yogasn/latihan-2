b73b7b2b976b53f7385230e2db60cd8feece9622
HafizHermawan <yoggnugraha@gmail.com>
2023-09-25 15:54:46 +0700

    ditambahkan oleh yoga

---
+++ pustaka-booking/application/controllers/lat2wp.php
@@ -0,0 +1,9 @@
+<?php 
+class lat2wp extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Yoga Surya Nugraha Saya tinggal di daerah Jakarta Barat olah raga yang saya sukai adalah Basket"; 
+    } 
+}
