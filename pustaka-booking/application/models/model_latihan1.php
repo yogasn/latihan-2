73b85d8d6563ebf1104c2fd805e95914f4835806
HafizHermawan <yoggnugraha@gmail.com>
2023-10-02 16:55:07 +0700

    telah ditambahkan oleh yoga

---
+++ pustaka-booking/application/models/model_latihan1.php
@@ -0,0 +1,13 @@
+<?php 
+class model_latihan1 extends CI_model
+{
+    public $nilai1, $nilai2, $hasil;
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        $this->hasil = $this->nilai1 + $this->nilai2;
+        return $this->hasil;
+    }
+
+}
