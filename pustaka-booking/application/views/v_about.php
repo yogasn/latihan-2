5ed72a4b7f71d0bf01cba47917dc9b3e1f560b28
HafizHermawan <yoggnugraha@gmail.com>
2023-10-09 16:08:25 +0700

    ditambahkan oleh yoga

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Yoga</li> 
+        <li>Nama Belakang : Surya Nugraha</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Inpres NO 51d Kebon Jeruk </li> 
+    </ul>
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Basket</li> 
+    </ul> 
+</section>
